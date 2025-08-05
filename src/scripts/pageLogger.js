9416db4cb97aa7eb29e34bf48805a7e85c3a8b24
raddevus <1radeutsch1@gmail.com>
2025-08-05 14:34:41 -0400

    Added fetch to make the call to save the basics via LogCap

--- src/scripts/pageLogger.js
+++ src/scripts/pageLogger.js
@@ -3,12 +3,13 @@
 function logPageView() {
   console.log("Navigated to:", window.location.pathname);
     const metadata = {
-    path: window.location.pathname,
-    fullURL: window.location.href,
+    path: window.location.pathname.replaceAll("/","-"),
+    fullURL: window.location.href.replaceAll("/","-"),
     referrer: document.referrer || "No referrer",
-    userAgent: navigator.userAgent,
+    userAgent: navigator.userAgent.replaceAll("/","-"),
   };
-
+   console.log(`UserAgent: ${metadata.userAgent}`);
+   fetch(`https://allos.dev/api/save/allos.dev/${metadata.path}/${metadata.userAgent}`);
   console.log("🔍 Page Metadata:", metadata);
 }
 
